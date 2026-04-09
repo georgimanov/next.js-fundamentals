8cd8cbbbd335dc28cdf13f7712ff081b490c066c
georgimanov <georgimanov@gmail.com>
2026-04-09 15:05:45 +0300

    Updated auth logic: pages, actions and dal

--- lib/dal.ts
+++ lib/dal.ts
@@ -4,3 +4,41 @@ import { eq } from 'drizzle-orm'
 import { cache } from 'react'
 import { issues, users } from '@/db/schema'
 import { mockDelay } from './utils'
+
+export const getUserByEmail = async (email: string) => {
+    try {
+        const users = await db
+        .select()
+        .from(users)
+        .where(eq(users.email, email))
+        
+        return users[0] || null
+        
+    } catch (error) {
+        console.error('Error fetching user by email:', error)
+        return null
+    }
+}
+
+export const getCurrentUser = async () => {
+    await mockDelay(500) // Simulate 
+    
+    // a delay for demonstration purposes
+    const session = await getSession();
+    if (!session) return null
+
+    try {
+        const users = await db
+        .select()
+        .from(users)
+        .where(eq(users.id, session.userId))
+        
+        return users[0] || null
+        
+    } catch (error) {
+        console.error('Error fetching user by email:', error)
+        return null
+    }
+    
+    return '';
+}
